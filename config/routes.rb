ba5691623ba31a6df0bfbd84980c2d73c254fe44
cebasypia <cebasypia.tskm8@gmail.com>
2020-04-24 12:02:02 +0900

    Add navbar, Tttle  ,root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  # TODO: fix url to login
+  root to: 'books#index'
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
