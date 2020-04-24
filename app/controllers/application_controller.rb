135778b3299157dc8cb228b311b78aa3ffe45808
cebasypia <cebasypia.tskm8@gmail.com>
2020-04-25 00:48:43 +0900

    Generate devise:controller user Add signed in check before every action If user is not signed in, then redirect to  a sign in page Change default route  to users/registrations Add home routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :user_logged_in?
+
+  def user_logged_in?
+    logger.debug('デバッグ')
+    if user_signed_in?
+    else
+      redirect_to new_user_registration_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # TODO: fix url to login
-  root to: 'books#index'
-  devise_for :users
+  root to: 'home#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :home, only: [:index]
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
