e7b547fcefa45535aadcdcf573efed65f9a8c298
cebasypia <cebasypia.tskm8@gmail.com>
2020-04-25 14:16:42 +0900

    Change how to redirect unauthenticated users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  skip_before_action :user_logged_in?
-
   def index; end
 
   def about; end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@
 
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-  resources :home, only: [:index]
+  devise_for :users
+  resources :home, only: %i[index]
   get 'home/about' => 'home#about'
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
