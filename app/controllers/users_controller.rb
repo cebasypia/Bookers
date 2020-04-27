7c3ca7e21208beaef9d48a32b081eadd7ef2531c
cebasypia <cebasypia.tskm8@gmail.com>
2020-04-27 18:16:56 +0900

    Separate sidebar into user_profile and new_book Move Book.new into _new_book Fix the grammar in render to render(partial:"",locals:{var:@var} )

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,10 @@
 
 class UsersController < ApplicationController
   def index
-    @book = Book.new
     @users = User.all
   end
 
   def show
-    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
   end
