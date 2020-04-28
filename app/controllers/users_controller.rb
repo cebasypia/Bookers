a1fb77e694c28b7c9c42799511989e6778e02582
cebasypia <cebasypia.tskm8@gmail.com>
2020-04-28 16:36:19 +0900

    Add users/:id/edit & update Add ensure correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,45 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update]
+  before_action :ensure_correct_user, only: %i[edit update]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
     @books = @user.books
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def ensure_correct_user
+    return if @user.id == current_user.id
+
+    flash[:notice] = '権限がありません'
+    redirect_to user_path(current_user.id)
   end
 end
